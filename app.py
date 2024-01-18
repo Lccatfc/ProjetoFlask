bd7c9f236d9487d98b7af4707296d0aa36191ae8
Lccatfc <ltfc102008@gmail.com>
2024-01-18 17:39:48 -0300

    cleancode

--- app.py
+++ app.py
@@ -3,26 +3,27 @@ import mysql.connector
 import json
 from mainmysql import myResult, insere
 
-
 app = Flask(__name__)
-
 
 @app.route("/inserir")
 def inserir():
   email = request.args.get('email')
   nome = request.args.get('nome')
   age = request.args.get('age')
+
   if 'email' not in request.args or 'nome' not in request.args or 'age' not in request.args:
     return 'Não tem dados'
+
   insere(email, nome, age)
   return 'você inseriu dados'
 
 @app.route("/exibir")
 def index():
   dados = request.args.get('coluna')
-  return json.dumps(myResult(dados))
-  return dados
+  if 'coluna' not in request.args:
+    return 'Não tem dados'
 
+  return json.dumps(myResult(dados))
 
 if __name__ == '__main__':
   app.run(debug=True)
--- mainmysql.py
+++ mainmysql.py
@@ -13,8 +13,6 @@ def insere(email, nome, age):
   mycursor.execute(f"INSERT INTO tabelanova (email, name, age) VALUES('{email}', '{nome}', '{age}')")
   mydb.commit()
 
-
-
 def myResult(coluna):
   mycursor.execute(f"SELECT {coluna} FROM tabelanova")
   myresult = mycursor.fetchall()
